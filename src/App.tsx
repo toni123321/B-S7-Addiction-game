d0008b4db8a50d207684e7c07938d6b90aceee62
Antonio Takev <tonitakev@gmail.com>
2023-12-06 16:39:50 +0100

    feat(design): Create the initial design of the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import MainActor from "./assets/mainActor.svg";
+import LevelBar from "./assets/levelBar.svg";
+import "./App.css";
+
+interface IFact {
+  title: string;
+  description: string;
+}
+const facts: IFact[] = [
+  {
+    title: "Fact #1",
+    description:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+  },
+  {
+    title: "Fact #2",
+    description:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+  },
+];
+
+function Fact({ title, description }: IFact) {
+  return (
+    <div className="fact">
+      <div>{title}</div>
+      <div>{description}</div>
+    </div>
+  );
+}
+
 function App() {
-  return <div>Hello from Addiction Game</div>;
+  return (
+    <div>
+      <div className="mainWrapper">
+        <div className="levelBarWrapper">
+          <img src={LevelBar} alt="Level bar" />
+        </div>
+        <div className="mainActorWrapper">
+          <img src={MainActor} alt="Main actor" />
+        </div>
+        <div className="factsList">
+          {facts.map((fact) => (
+            <Fact title={fact.title} description={fact.description} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
